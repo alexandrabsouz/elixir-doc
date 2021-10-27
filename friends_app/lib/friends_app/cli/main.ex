a35fb6ff5224a7762cce070730d35ac10a0bd58f
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-26 22:32:07 -0300

    feat: mostrando um menu

--- friends_app/lib/friends_app/cli/main.ex
+++ friends_app/lib/friends_app/cli/main.ex
@@ -1,15 +1,20 @@
 defmodule FriendsApp.CLI.Main do
-  alias Mix.Shell.IO as Shell
+  alias Mix.Shell.IO, as: Shell
 
   def start_app do
     Shell.cmd("Clear")
-    welcome_message
+    welcome_message()
     Shell.prompt("Pressione ENTER para iniciar...")
+    start_menu_choice()
   end
 
-  def welcome_message do
+  defp welcome_message do
     Shell.info("============ Friends App ============")
     Shell.info(" Seja bem vindo a sua agenda pessoal!")
     Shell.info("=====================================")
+  end
+
+  defp start_menu_choice do
+    FriendsApp.CLI.MenuChoice.start
   end
 end
---
+++ friends_app/lib/friends_app/cli/menu_itens.ex
@@ -0,0 +1,10 @@
+defmodule FriendsApp.CLI.MenuItens do
+  alias FriendsApp.CLI.Menu
+
+  def all, do: [
+    %Menu{label: "Cadastra os amigos", id: :create},
+    %Menu{label: "Listar os amigos", id: :read},
+    %Menu{label: "Atualiza um amigo", id: :update},
+    %Menu{label: "Exclui um amigo", id: :delete}
+  ]
+end
