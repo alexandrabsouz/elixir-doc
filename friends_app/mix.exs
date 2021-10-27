5c60a0a69e2c9a1dc0a5e9c94eacd3fe026c6916
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-27 15:57:27 -0300

    feat: listando amigos cadastrados

--- friends_app/mix.exs
+++ friends_app/mix.exs
@@ -22,7 +22,8 @@ defmodule FriendsApp.MixProject do
   defp deps do
     [
       {:nimble_csv, "~> 1.1"},
-      {:faker, "~> 0.16.0"}
+      {:faker, "~> 0.16.0"},
+      {:scribe, "~> 0.10"}
     ]
   end
 end
